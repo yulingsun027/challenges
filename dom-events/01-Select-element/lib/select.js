37c05bb78b7467ce2eb7228e91d6d56908baa741
yuling sun <yulingsun027@hotmail.com>
2020-07-09 21:42:23 +0800

    print out the selected country

---
+++ dom-events/01-Select-element/lib/select.js
@@ -0,0 +1,14 @@
+// This will check if you're able to run javascript.
+// If you don't see a popup greeting you, talk to a teacher!
+
+alert("Hello! You are running Javascript")
+
+// TODO 1. Open the `index.html` file and set an id attribute to the China <li>
+// (nothing to write in this file for this step, write it in the html file)
+
+
+// TODO 2: select the China list item and return it!
+
+
+const flaggedCountry = document.querySelector('#flagged');
+console.log(flaggedCountry);
